420c929020c91ff08d7e343a95925e9b584d2ef3
h.ayabe <h.ayabe_biz@live.jp>
2024-04-29 12:48:06 +0900

    pr

--- frontend-web/islands/LazyLoadOGP.tsx
+++ frontend-web/islands/LazyLoadOGP.tsx
@@ -5,18 +5,23 @@ export function LazyLoadOGP(
 ) {
   signal.value.forEach(async (pr) => {
     if (pr.image) return;
-    const res = await fetch(pr.url, {
-      headers: {
-        "User-Agent": "robot",
-      },
-    });
-    const html = await res.text();
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, "text/html");
-    const ogp = doc.querySelector("meta[property='og:image']");
-    if (ogp) {
-      // TODO convert to base64 string data
-      pr.image = ogp.getAttribute("content") ?? undefined;
+    try {
+      const res = await fetch(pr.url, {
+        headers: {
+          "User-Agent": "robot",
+        },
+      });
+      const html = await res.text();
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, "text/html");
+      const ogp = doc.querySelector("meta[property='og:image']");
+      if (ogp) {
+        // TODO convert to base64 string data
+        pr.image = ogp.getAttribute("content") ?? undefined;
+      }
+    } catch (e) {
+      // ignore errors
+      console.error(e);
     }
   });
   return <></>;
