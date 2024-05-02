1cbfd33373595adb52836b0a333da681974e9a03
h.ayabe <h.ayabe_biz@live.jp>
2024-05-02 15:24:16 +0900

    fix

--- frontend-web/data/2024-05/index.ts
+++ frontend-web/data/2024-05/index.ts
@@ -1,13 +1,10 @@
 import data20240501 from "./2024-05-01.ts";
 import data20240502 from "./2024-05-02.ts";
-import data20240503 from "./2024-05-03.ts";
-import data20240504 from "./2024-05-04.ts";
 
 const dailyData = {
   "2024-05-01": data20240501,
   "2024-05-02": data20240502,
-  "2024-05-03": data20240503,
-  "2024-05-04": data20240504,
 };
 
 export default dailyData;
+F;
