6e2f0441e7ae417c32e08862ca22e65b41f2aaaf
h.ayabe <h.ayabe_biz@live.jp>
2024-05-06 13:16:40 +0900

    notranslate title

--- frontend-web/routes/_app.tsx
+++ frontend-web/routes/_app.tsx
@@ -2,7 +2,7 @@ import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component, config }: PageProps) {
   return (
     <html lang="en">
-      <head className={"notranslate"}>
+      <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
@@ -15,7 +15,7 @@ export default function App({ Component, config }: PageProps) {
         />
         <meta name="author" content="AY.LLC" />
         <meta name="robots" content="index, follow" />
-        <title>
+        <title className={"notranslate"}>
           Daily AI-Generated English Novels & Learning Test Questions
         </title>
         <link
