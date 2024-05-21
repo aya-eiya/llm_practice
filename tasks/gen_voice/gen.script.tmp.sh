61d8648e8e426c2737e42857ed3a9c8e2beb4791
h.ayabe <h.ayabe_biz@live.jp>
2024-05-21 16:59:56 +0900

    wip

--- tasks/gen_voice/gen.script.tmp.sh
+++ tasks/gen_voice/gen.script.tmp.sh
@@ -5,7 +5,7 @@ inputfile="../gen_quiz/outputs/${fulldate}.json"
 
 jq '[{"System": "Improve Your English Skills with Daily AI-Generated Novels and Learning Tests."}, {"System": .title }]' "${inputfile}" > /tmp/gen_voice_header.json
 jq '(.body|gsub("(?<a>[.?!]+)(?<b>.)"; "\(.a)[!SENT!]\(.b)"))|split("[!SENT!]")|map({"Narrator":.|gsub("^ +| +$"; "")})' "${inputfile}" > /tmp/gen_voice_body.json
-jq -n "[{\"System\": \"Now, let's move on to the conversation between Billy, Meg, Kelly, and their teacher, Louis, after reading the novel.\"}]" > /tmp/gen_voice_pre_dialog.json
+jq -n "[{\"System\": \"Now, let's move on to the conversation between Billy, Meg, Kelly, and their teacher, Lui, after reading the novel.\"}]" > /tmp/gen_voice_pre_dialog.json
 jq '.dialog' "${inputfile}" > /tmp/gen_voice_dialog.json
 jq -n "[{ \"System\": \"Now, let's test your understanding of the novel and the dialogue with 5 questions.\" }]" > /tmp/gen_voice_post_dialog.json
 jq '.quiz | to_entries | map([
