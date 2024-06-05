e33f42e3b88c3e128de7a0870fef2d9444789d09
h.ayabe <h.ayabe_biz@live.jp>
2024-06-06 00:22:44 +0900

    gen

--- tasks/gen_voice/gen.short-script.tmp.sh
+++ tasks/gen_voice/gen.short-script.tmp.sh
@@ -8,18 +8,20 @@ curl -X GET "http://localhost:8000/bookshelf?date=${fulldate}&img=jpg" --output 
 
 jq -s "[
   { \"Effect\": \"silence=1000,fade_music=[\\\"musics/Opening_2.mp3\\\",duration=:header,start=0,fade=2000]\" },
-  { \"Effect\": \"silence=1000,talk_speed=1.1\" },
-  { \"System\": \"My-niq Short listening challenge!\" },
+  { \"Effect\": \"silence=1000,talk_speed=1.0\" },
+  { \"System\": \"My-niq Short Listening Challenge!\" },
   { \"System\": \"Let's listen to the following text and try to fill in the blank words in the subtitles!\" },
-  { \"Narrator\": .[0].title }
-  { \"Effect\": \"silence=500,label=:header\" },
+  { \"Effect\": \"silence=1000,talk_speed=1.1\" },
+  { \"Narrator\": .[0].title },
+  { \"Effect\": \"silence=500,label=:header\" }
 ]" "${inputfile}" "${curriculum}" > /tmp/gen_voice_header.json
 python scripts/segment_body.py "$(jq -r -c '.body' ${inputfile} )" | jq '. | map({"Narrator":.|gsub("^ +| +$"; "")|gsub("&npsp;"; " ")})' > /tmp/gen_voice_body.json
 jq -n "[
   { \"Effect\": \"silence=500,fade_music=[\\\"musics/Ending_1.mp3\\\",duration=:last,start=0,fade=4000]\" },
-  { \"System\": \"To find out the correct answers for the blanks, check out the videos on our channel.\",
+  { \"Effect\": \"silence=3000,talk_speed=1.0\" },
+  { \"System\": \"To find out the correct answers for the blanks, check out the videos on our channel.\" },
   { \"System\": \"Don't forget to subscribe for daily content that will help you improve your English!\" },
-  { \"Effect\": \"silence=15000\" },
+  { \"Effect\": \"silence=4000\" },
   { \"Effect\": \"silence=3000,label=:last\" }
 ]" > /tmp/gen_voice_footer.json
 jq -s 'flatten' \
