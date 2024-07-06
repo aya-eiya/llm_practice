fc4d68990a9f58c3ddc19594a820f9609ac72c59
h.ayabe <h.ayabe_biz@live.jp>
2024-07-06 15:19:57 +0900

    gen

--- frontend-web/scripts/generate_voice_list.sh
+++ frontend-web/scripts/generate_voice_list.sh
@@ -6,12 +6,26 @@ if [ "${API_KEY}" = "" ]; then
 fi
 API="https://www.googleapis.com/youtube/v3"
 PLAY_LIST_ID="PLe8rBiAN890Y9055UfPQhNWFD_A8dr9Ro"
+pageToken=""
+video_ids="{}"
 
-video_ids=$(curl -X GET \
-  "${API}/playlistItems?key=${API_KEY}&part=snippet&maxResults=51&playlistId=${PLAY_LIST_ID}" 2>/dev/null \
-  | jq '
-    .items
-    | map(
+while true; do
+  case "$pageToken" in
+    "")
+      res=$(curl -X GET "${API}/playlistItems?key=${API_KEY}&part=snippet&maxResults=50&playlistId=${PLAY_LIST_ID}" 2>/dev/null)
+      ;;
+    "null")
+      break
+      ;;
+    *)
+      res=$(curl -X GET "${API}/playlistItems?key=${API_KEY}&part=snippet&maxResults=50&playlistId=${PLAY_LIST_ID}&pageToken=${pageToken}" 2>/dev/null)
+      ;;
+  esac
+  pageToken=$(echo "$res" | jq -r '.nextPageToken | if . == null then "null" else . end')
+  echo "pageToken: $pageToken"
+  _video_ids=$(echo "$res" \
+  | jq -s '
+    .[0].items | map(
       {
         "value": {
           "id": .snippet.resourceId.videoId,
@@ -23,6 +37,9 @@ video_ids=$(curl -X GET \
       }
     ) | from_entries'\
   )
+  video_ids=$(echo "$video_ids $_video_ids" | jq -s 'add')
+done
+
 
 echo "const videoData = $video_ids as const" > "data/videoData.ts"
 echo "
