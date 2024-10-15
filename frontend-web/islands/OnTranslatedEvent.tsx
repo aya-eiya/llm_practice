7e6d42786f1205b4872e2a29d726c1bf4df49a2a
h.ayabe <h.ayabe_biz@live.jp>
2024-10-16 05:41:45 +0900

    fix

--- frontend-web/islands/OnTranslatedEvent.tsx
+++ frontend-web/islands/OnTranslatedEvent.tsx
@@ -10,7 +10,7 @@ export default function OnTranslatedEvent(
       if (onReload) {
         onReload.value = new Date().getTime();
       } else {
-        window.location.reload();
+        globalThis.window.location.reload();
       }
     });
     const translationObserver = new MutationObserver((e) => {
