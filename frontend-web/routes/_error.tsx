19170701c731ff9e475df183655438d5f14637cf
h.ayabe <h.ayabe_biz@live.jp>
2025-11-28 11:29:01 +0900

    update

--- frontend-web/routes/_error.tsx
+++ frontend-web/routes/_error.tsx
@@ -1,15 +1,20 @@
 import { HttpError, PageProps } from "fresh";
-import Error404 from "../components/errors/_404.tsx";
+import Error404 from "../components/errors/404.tsx";
+import Error from "../components/errors/Error.tsx";
 
 export default function ErrorPage(props: PageProps) {
   const error = props.error; // Contains the thrown Error or HTTPError
+  let status = 200;
   if (error instanceof HttpError) {
-    const status = error.status; // HTTP status code
+    status = error.status; // HTTP status code
 
     // Render a 404 not found page
     if (status === 404) {
       return Error404(props);
     }
   }
-  return <pre>{JSON.stringify(props.error)}</pre>;
+  if (props.config.mode === "development") {
+    return <textarea>{JSON.stringify(props.error)}</textarea>;
+  }
+  return Error(props);
 }
