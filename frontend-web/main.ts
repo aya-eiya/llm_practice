ac49f66971d7e9bbacacefbe117b4c2cc0bc2caf
h.ayabe <h.ayabe_biz@live.jp>
2026-05-13 19:02:39 +0900

    update

--- frontend-web/main.ts
+++ frontend-web/main.ts
@@ -19,13 +19,14 @@ export const app = new App<{ lang: string }>()
   })
   .use((ctx) => {
     console.log(ctx.info.remoteAddr, ctx.url);
-    if (ctx.url.host.startsWith("aya-eiya.work")) {
+    if (ctx.url.host.startsWith("aya-eiya.work") && ctx.url.pathname === "/") {
       const url = new URL(ctx.url);
       url.host = "www.aya-eiya.work";
       return ctx.redirect(url.toString(), 301);
     }
     if (
-      ctx.url.host.includes("deno.dev")
+      ctx.url.host.includes("deno.dev") ||
+      ctx.url.host.startsWith("aya-eiya.work")
     ) {
       const url = new URL(ctx.url);
       url.host = "myniq.aya-eiya.work";
