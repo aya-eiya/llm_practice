fb041a970bb586c6a7b2517bf94947e9af64ba9f
h.ayabe <h.ayabe_biz@live.jp>
2026-07-11 12:37:53 +0900

    add google ads type

--- frontend-web/pr/index.ts
+++ frontend-web/pr/index.ts
@@ -7,14 +7,14 @@ const PRs: PR[] = [
     provider: "A8",
     url: "https://px.a8.net/svt/ejp?a8mat=3Z6VU2+6KD4PM+3AQG+TVBF6",
     title: "Study Supplie for Buisiness English",
-    image: images?.["A8N001"] || "", // JSONから安全に取得
+    image: images?.["A8N001"],
   },
   {
     id: "A8N002",
     provider: "A8",
     title: "my.LunCul.com",
     url: "https://px.a8.net/svt/ejp?a8mat=3Z6VU2+6F089M+4FL0+BY641",
-    image: images?.["A8N002"] || "", // JSONから安全に取得
+    image: images?.["A8N002"],
   },
   {
     id: "AMZ001",
@@ -22,7 +22,7 @@ const PRs: PR[] = [
     title:
       "Large Language Model-Based Solutions: How to Deliver Value with Cost-Effective Generative AI Applications (Tech Today)",
     url: "https://amzn.to/3JE9Zr5",
-    image: images?.["AMZ001"] || "", // JSONから安全に取得
+    image: images?.["AMZ001"],
   },
   {
     id: "AMZ002",
@@ -30,14 +30,14 @@ const PRs: PR[] = [
     title:
       "Julia makes new friends (No worries - short bedtime stories) (English Edition)",
     url: "https://amzn.to/4aRwtAY",
-    image: images?.["AMZ002"] || "", // JSONから安全に取得
+    image: images?.["AMZ002"],
   },
   {
     id: "AMZ003",
     provider: "Amazon",
     title: "Audible membership plan 2 months free trial",
     url: "https://amzn.to/4aZwa75",
-    image: images?.["AMZ003"] || undefined, // JSONから安全に取得 (undefined許容)
+    image: images?.["AMZ003"],
   },
 ];
 export default PRs;
